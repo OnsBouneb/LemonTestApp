a927bdf57d11496e9a48ed4acc2c7d0bc79fcb0e
unknown <onsbouneb1@gmail.com>
2020-01-12 14:15:51 +0100

    first committ

---
+++ LemonTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+<Assembly: AssemblyTitle("LemonTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LemonTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("5649e4fc-544f-4f0f-b125-f0ee0e50729b")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ LemonTest/WebService1.asmx.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.ComponentModel
+
+' Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<ToolboxItem(False)> _
+Public Class WebService1
+    Inherits System.Web.Services.WebService
+
+    <WebMethod()>
+    Public Function HelloWorldOns() As String
+        Return "Hello World Ons"
+    End Function
+
+    <WebMethod()>
+    Public Function HelloWorld(j As String
+    ) As String
+        Return "Hello World" + j
+    End Function
+
+    <WebMethod()>
+    Public Function Fibo(j As Integer) As Integer
+
+        If (j < 2) Then
+            Return j
+
+        End If
+        Dim FibArray(2) As Integer
+        Dim i As Integer
+        FibArray(0) = 0
+        FibArray(1) = 1
+        For i = 2 To j
+            FibArray(1) = FibArray(1) + FibArray(0)
+            FibArray(0) = FibArray(1) - FibArray(0)
+        Next
+        Return FibArray(1)
+
+    End Function
+
+
+
+End Class
