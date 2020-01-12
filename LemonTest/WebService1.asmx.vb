a270f4713f5ee87a129d13c72b8cb13e804a2c36
unknown <onsbouneb1@gmail.com>
2020-01-12 17:04:01 +0100

    added the function XmlToJson

--- LemonTest/WebService1.asmx.vb
+++ LemonTest/WebService1.asmx.vb
@@ -1,28 +1,19 @@
 ﻿Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
+Imports System.Xml
+Imports System.Web.Http
 
-' Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante.
-' <System.Web.Script.Services.ScriptService()> _
-<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
-<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
-<ToolboxItem(False)> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
 Public Class WebService1
     Inherits System.Web.Services.WebService
 
-    <WebMethod()>
-    Public Function HelloWorldOns() As String
-        Return "Hello World Ons"
-    End Function
+    Public Property JsonConvert As Object
 
     <WebMethod()>
-    Public Function HelloWorld(j As String
-    ) As String
-        Return "Hello World" + j
-    End Function
-
-    <WebMethod()>
-    Public Function Fibo(j As Integer) As Integer
+    Public Function Fibonacci(j As Integer) As Integer
         If (Not (j < 0 Or j > 100)) Then
 
             If (j < 2) Then
@@ -43,6 +34,22 @@ Public Class WebService1
         End If
     End Function
 
+    <WebMethod()>
+    Public Function XmlToJson(xml As String) As String
+
+
+        Dim doc As XmlDocument
+        doc.LoadXml("<foo>bar</foo>")
+        Dim json As String
+
+
+        json = JsonConvert.SerializeXmlNode(doc)
+
+        Console.WriteLine(json)
+        Console.WriteLine("ons")
+        Return json
+
+    End Function
 
 
 End Class
